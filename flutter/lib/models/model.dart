e7cade8b0a15926b9a518d2873594a14b18916e3
XHead-git <fullbanner@gmail.com>
2025-03-28 15:36:07 +0000

    Vem do sync

--- flutter/lib/models/model.dart
+++ flutter/lib/models/model.dart
@@ -2612,6 +2612,8 @@ class CursorModel with ChangeNotifier {
     _x = -10000;
     _x = -10000;
     _image = null;
+    _firstUpdateMouseTime = null;
+    gotMouseControl = true;
     disposeImages();
 
     _clearCache();
