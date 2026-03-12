5928d793a94b7dab2b811435c5637f5b37771944
XHead-git <38805138+XHead-git@users.noreply.github.com>
2026-03-12 22:48:15 -0100

    Refactor using_public_server function logic

--- src/common.rs
+++ src/common.rs
@@ -1733,7 +1733,8 @@ pub fn create_symmetric_key_msg(their_pk_b: [u8; 32]) -> (Bytes, Bytes, secretbo
 
 #[inline]
 pub fn using_public_server() -> bool {
-    crate::get_custom_rendezvous_server(get_option("custom-rendezvous-server")).is_empty()
+    option_env!("RENDEZVOUS_SERVER").unwrap_or("").is_empty()
+        && crate::get_custom_rendezvous_server(get_option("custom-rendezvous-server")).is_empty()
 }
 
 pub struct ThrottledInterval {
